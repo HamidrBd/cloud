a0e26e3e2b084b60b9fc1da1da89840925b3ce2d
Hamidreza Badr <84298452+HamidrBd@users.noreply.github.com>
2023-08-27 11:20:47 +0330

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import streamlit as st
 from st_files_connection import FilesConnection
 
+st.write('hamidreza badr')
+
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('gcs', type=FilesConnection)
