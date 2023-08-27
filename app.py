dcbf0161c29bd37dd4ad2b2e20e1a5f5832d9a53
Hamidreza Badr <84298452+HamidrBd@users.noreply.github.com>
2023-08-27 12:51:27 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ st.write('hamidreza badr')
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('gcs', type=FilesConnection)
+st.write('hamidreza badr')
+
 df = conn.read("streamlit-bucket-hamid/myfile.csv", input_format="csv", ttl=600)
+st.write('reza badr ')
 
 # Print results.
 for row in df.itertuples():
